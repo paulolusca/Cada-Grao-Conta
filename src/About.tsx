58730fa6a385c16e628727cd2303f7b048bc6e9d
Paulo Lima <72480129+paulolusca@users.noreply.github.com>
2025-10-20 23:19:59 -0300

    Update About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -58,8 +58,8 @@ const About = () => {
         <div className="aspect-w-16 aspect-h-9">
           <iframe
               className="w-full h-full rounded-lg"
-              src="https://www.youtube.com/embed"
-              title="Como Aproveitar 100% dos Alimentos"
+              src="https://youtube.com/shorts/TuMRZOhfS70"
+              title="Dia da Conscientização contra o Desperdício de Alimentos e lançamento do aplicativo Cada Grão Conta"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
@@ -68,8 +68,8 @@ const About = () => {
         <div className="aspect-w-16 aspect-h-9">
           <iframe
               className="w-full h-full rounded-lg"
-              src="https://www.youtube.com/embed"
-              title="Compostagem Caseira: Passo a Passo"
+              src="https://youtube.com/shorts/TuMRZOhfS70"
+              title="Cada Grão Conta"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
