98dd4675c58dd88b14c8d5d8a11b77a48a75a0f2
Paulo Lima <72480129+paulolusca@users.noreply.github.com>
2025-10-20 23:32:32 -0300

    Update About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -58,7 +58,7 @@ const About = () => {
         <div className="aspect-w-16 aspect-h-9">
           <iframe
               className="w-full h-full rounded-lg"
-              src="https://www.youtube.com/embed/fxmBcrb-kHk?si=siSw8WcuRNEiwGir"
+              src="https://www.youtube.com/embed/TuMRZOhfS70?si=5hGuwrWVbZJcuJf-"
               title="Dia Internacional da Conscientização contra o Desperdício de Alimentos e lançamento do aplicativo Cada Grão Conta"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
